1e3f8ba30a83190b65d50934dae89595136af189
Caio Ramos <caio.ramos@smartfit.com>
2025-02-28 23:20:00 -0300

    Ajusta carregamento do JS do mapa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    # Buscar todas as reclamações com coordenadas válidas
+    @complaints = Complaint.where.not(latitude: nil, longitude: nil)
+                         .order(created_at: :desc)
   end
 end
