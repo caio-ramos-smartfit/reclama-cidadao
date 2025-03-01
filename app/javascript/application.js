54e0a8cf8cb2c4e96153f9ed1bb5e2e1313c8403
MartinsMarco <marco.martins@bioritmo.com.br>
2025-03-01 00:42:53 -0300

    FIx js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener('turbo:before-frame-render', () => {
+    console.log("load aqui")
+    const dropdownToggle = document.getElementById('navbarDropdown');
+    dropdownToggle.addEventListener('click', (event) => {
+        event.preventDefault(); // Previne o comportamento padrão
+        // Aqui você pode adicionar a lógica para atualizar o conteúdo do dropdown, se necessário
+        // Exemplo: re-inicializar o dropdown do Bootstrap
+        const dropdown = new bootstrap.Dropdown(dropdownToggle);
+        dropdown.toggle(); // Alterna o estado do dropdown
+    });
+});
