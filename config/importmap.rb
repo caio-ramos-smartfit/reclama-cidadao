57b5f2a56367303b67dfecec92dfe566559f3725
Caio Ramos <caio.ramos@smartfit.com>
2025-03-02 15:28:44 -0300

    Muitos fixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "complaint_map_initializer", to: "complaint_map_initializer.js"
+pin "map_manager", to: "map_manager.js"
