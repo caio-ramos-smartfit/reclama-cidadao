5a0b182fc561e13e27aaca73f06398b844390dde
Caio Ramos <caio.ramos@smartfit.com>
2025-03-03 13:50:57 -0300

    Ajusta layout da home

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,22 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "map_manager"
+import "home_animations"
 
 document.addEventListener('turbo:before-frame-render', () => {
     console.log("load aqui")
     const dropdownToggle = document.getElementById('navbarDropdown');
-    dropdownToggle.addEventListener('click', (event) => {
-        event.preventDefault(); // Previne o comportamento padrão
-        // Aqui você pode adicionar a lógica para atualizar o conteúdo do dropdown, se necessário
-        // Exemplo: re-inicializar o dropdown do Bootstrap
-        const dropdown = new bootstrap.Dropdown(dropdownToggle);
-        dropdown.toggle(); // Alterna o estado do dropdown
-    });
+    if (dropdownToggle) {
+        dropdownToggle.addEventListener('click', (event) => {
+            event.preventDefault(); // Previne o comportamento padrão
+            // Aqui você pode adicionar a lógica para atualizar o conteúdo do dropdown, se necessário
+            // Exemplo: re-inicializar o dropdown do Bootstrap
+            if (typeof bootstrap !== 'undefined') {
+                const dropdown = new bootstrap.Dropdown(dropdownToggle);
+                dropdown.toggle(); // Alterna o estado do dropdown
+            }
+        });
+    }
 });
 
 // Adicionar eventos para detectar navegação para a home
